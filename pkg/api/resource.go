dfacfe85c7188329a1dd3d93fb94538f0ae3f8ac
tithakka <tithakka@redhat.com>
2026-05-26 17:04:57 -0500

    HYPERFLEET-1084 - feat: Fix linter

--- pkg/api/resource.go
+++ pkg/api/resource.go
@@ -36,7 +36,8 @@ type (
 	ResourceIndex map[string]*Resource
 )
 
-// TODO: Evaluate the need for this method as part of https://redhat.atlassian.net/browse/HYPERFLEET-1085 and remove if not needed
+// TODO: Evaluate the need for this method as part of
+// https://redhat.atlassian.net/browse/HYPERFLEET-1085 and remove if not needed
 func (l ResourceList) Index() ResourceIndex {
 	index := ResourceIndex{}
 	for _, o := range l {
